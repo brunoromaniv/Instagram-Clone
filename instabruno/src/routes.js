7395cb71c7cbe7c51f95cb11484bfd77178679ca
Bruno Romaniv <brunoromaniv131@hotmail.com>
2019-06-22 15:38:14 -0300

    Restos dos arquivos

---
+++ instabruno/src/routes.js
@@ -0,0 +1,26 @@
+import {createAppContainer, createStackNavigator} from 'react-navigation';
+import React from 'react';
+import {Image} from 'react-native';
+
+import Feed from './pages/Feed';
+import New from './pages/New';
+
+import logo from './assets/logo.png'
+
+export default createAppContainer(
+    createStackNavigator({
+        Feed,
+        New,
+
+    }, {
+        //initialRouteName: 'New',
+        defaultNavigationOptions: {
+            hearderTintColor: '#000',
+            headerTitle: <Image style={{ marginHorizontal:20 }} source = {logo} />,
+            headerBackTitle: null,
+        },
+        mode:'modal'
+     
+    
+    })
+);
